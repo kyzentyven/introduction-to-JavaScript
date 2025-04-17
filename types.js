37cd41844b524904617436344bba841f7f1116d5
kyzentyven <kyzentyven@gmail.com>
2025-04-17 13:36:35 +0600

    Added the types of variables and output examples.

---
+++ types.js
@@ -0,0 +1,21 @@
+// String type variable
+var name = "Tuhin";
+console.log(name);
+
+var address = "Dhaka";
+var country = "Bangladesh";
+console.log(address);
+console.log(country);
+console.log(typeof country);
+
+// Boolean type variable
+var isRich = true;
+console.log(isRich);
+
+var passed = false;
+console.log(passed);
+console.log(typeof passed);
+
+// Numeric type variable
+var price = 1000;
+console.log(typeof price);
