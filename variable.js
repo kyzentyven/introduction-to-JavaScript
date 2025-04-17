47a7947bf0d22e314323594ce8c2be3c169fb342
kyzentyven <kyzentyven@gmail.com>
2025-04-17 13:32:58 +0600

    Added variable declaration with output example.

---
+++ variable.js
@@ -0,0 +1,11 @@
+var weight = 80;
+var price = 100;
+var year = 2002;
+var money = 2000;
+money = 1000;
+
+console.log(money);
+console.log(price);
+
+var age; // Undefined variable
+console.log(age);
