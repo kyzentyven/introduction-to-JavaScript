1540db379b145641d5bf6eeedb70531c9e666403
kyzentyven <kyzentyven@gmail.com>
2025-04-17 14:14:30 +0600

    Added arithmetic operations with output examples.

---
+++ shorthand.js
@@ -0,0 +1,23 @@
+// Addition
+var price = 35;
+// price = price + 10;
+price += 10; // price = price + 10;
+console.log(price);
+
+//Subtraction
+var fridge = 35000;
+// fridge = fridge - 1000;
+fridge -= 1000; // fridge = fridge - 1000;
+console.log(fridge);
+
+// Multiplication
+var orangePrice = 30;
+// orangePrice = orangePrice * 3;
+orangePrice *= 3; // orangePrice = orangePrice * 3;
+console.log(orangePrice);
+
+// Division
+var potatoPrice = 100;
+// potatoPrice = potatoPrice / 5;
+potatoPrice /= 5; // potatoPrice = potatoPrice / 5;
+console.log(potatoPrice);
